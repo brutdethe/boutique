3c9feb21cc9049676c770cddaaee249421875c6b
pntbr <pntbr@pm.me>
2020-11-14 00:43:10 +0100

    tranlate interface

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,11 +17,32 @@
   let lang = "fr";
   let category = "Gaiwan";
 
-  const categoriesText = {
-    Gaiwan: "Le gaiwan ou zhong, est l'ustensile idéal",
-    Théière:
-      "Chaque théière que je fabrique, représente une pièce unique dans laquelle je m'implique et je m'applique.",
-    Tasse: "Sans tasse on ne pourrait pas boire le thé."
+  const dict = {
+    title: { en: "Jé's little store", fr: "La petite boutique de Jé" },
+    buy: {
+      en: "buy",
+      fr: "acheter"
+    },
+    currency: {
+      en: "$",
+      fr: "€"
+    },
+    categoriesText: {
+      Gaiwan: {
+        en: "The gaiwan or zhong is the ideal utensil.",
+        fr: "Le gaiwan ou zhong, est l'ustensile idéal"
+      },
+      Théière: {
+        en:
+          "Each teapot I make represents a unique piece in which I am involved and apply myself.",
+        fr:
+          "Chaque théière que je fabrique, représente une pièce unique dans laquelle je m'implique et je m'applique."
+      },
+      Tasse: {
+        en: "Without a cup we couldn't drink tea.",
+        fr: "Sans tasse on ne pourrait pas boire le thé."
+      }
+    }
   };
 
   items.find = function(query) {
@@ -76,7 +97,7 @@
   <title>La petite boutique de Jé</title>
 </svelte:head>
 
-<h1>La petite boutique de Jé</h1>
+<h1>{dict.title[lang]}</h1>
 
 <section class="container">
   <hr />
@@ -105,7 +126,7 @@
   <div class="hero-sm bg-gray">
     <div class="hero-body">
       <h3>{category}</h3>
-      <p>{categoriesText[category]}</p>
+      <p>{dict.categoriesText[category][lang]}</p>
     </div>
   </div>
   <div class="docs-demo columns">
@@ -124,11 +145,11 @@
           </div>
           <div class="card-body">
             {item.description[lang]} - {item.poids} g -
-            <h3>{item.prix} €</h3>
+            <h3>{item.prix} {dict.currency[lang]}</h3>
           </div>
           <div class="card-footer">
             <div class="btn-group btn-group-block">
-              <button class="btn btn-primary">Acheter</button>
+              <button class="btn btn-primary">{dict.buy[lang]}</button>
             </div>
           </div>
         </div>
