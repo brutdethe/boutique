5dac109c953eb67d0750fc40fffcad25eb663024
pntbr <pntbr@pm.me>
2020-11-14 14:28:50 +0100

    initializes product page

---
+++ src/routes/produit-[titre].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  export function preload(page) {
+    return { titre: page.params.titre };
+  }
+</script>
+
+<script>
+  export let titre;
+</script>
+
+<svelte:head>
+  <title>{titre}</title>
+</svelte:head>
+
+<h1>{titre}</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
