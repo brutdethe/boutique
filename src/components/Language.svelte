6726e2808007bc2b35e58292cfd47c290c57616b
pntbr <pntbr@pm.me>
2020-11-21 18:47:22 +0100

    redesigns language menu

--- src/components/Language.svelte
+++ src/components/Language.svelte
@@ -5,21 +5,29 @@
     languageSelected.set(evt.currentTarget.value);
 </script>
 
-<article>
-  <label>
+<style>
+  .form-group {
+    line-height: 4em;
+  }
+</style>
+
+<div class="form-group column col-1 ">
+  <label class="form-radio form-inline">
     <input
       type="radio"
       bind:group={$languageSelected}
       on:change={changeLanguageSelected}
       value="en" />
+    <i class="form-icon" />
     EN
   </label>
-  <label>
+  <label class="form-radio form-inline">
     <input
       type="radio"
       bind:group={$languageSelected}
       on:change={changeLanguageSelected}
       value="fr" />
+    <i class="form-icon" />
     FR
   </label>
-</article>
+</div>
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -17,13 +17,6 @@
     line-height: 4rem;
   }
 
-  .language {
-    font-size: 1.5rem;
-    color: #777;
-    font-weight: 700;
-    line-height: 4rem;
-  }
-
   nav {
     border-bottom: 1px solid rgba(255, 62, 0, 0.1);
     font-weight: 300;
@@ -47,21 +40,6 @@
     float: left;
   }
 
-  [aria-current] {
-    position: relative;
-    display: inline-block;
-  }
-
-  [aria-current]::after {
-    position: absolute;
-    content: "";
-    width: calc(100% - 1em);
-    height: 2px;
-    background-color: rgb(255, 62, 0);
-    display: block;
-    bottom: -1px;
-  }
-
   a {
     text-decoration: none;
     padding: 1em 0.5em;
@@ -72,17 +50,14 @@
 <nav class="columns">
   <ul class="column col-4">
     <li>
-      <a
-        aria-current={segment === undefined ? 'page' : undefined}
-        href="./produits.json">
-        produits.json
-      </a>
+      <a href="./produits.json">produits.json</a>
+    </li>
+    <li>
+      <a href="./categories.json">categories.json</a>
     </li>
   </ul>
   <h1 class="title column col-6">
     <a href="/">{dict.title[$languageSelected]}</a>
   </h1>
-  <div class="language">
-    <Language />
-  </div>
+  <Language />
 </nav>
