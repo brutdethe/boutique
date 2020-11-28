4cd185fc9928a3b5c8e6e14505e60c22f90d1c0e
pntbr <pntbr@pm.me>
2020-11-28 11:34:16 +0100

    add stock

--- src/components/Buy.svelte
+++ src/components/Buy.svelte
@@ -3,9 +3,40 @@
 
   export let item;
 
-  function basketClick(evt) {
-    item.qty = 1;
-    basket.set([...$basket, item]);
+  $: stock = calculStock();
+
+  function calculStock() {
+    const basketItem = $basket.filter(product => product.id === item.id);
+    if (basketItem[0]) {
+      console.log("+item.stock", +item.stock);
+      console.log("+item.qty", basketItem[0]);
+      return +item.stock - +basketItem[0].qty;
+    } else {
+      console.log("+item.stock", +item.stock);
+      console.log("+item.qty", basketItem[0]);
+      return item.stock;
+    }
+  }
+
+  function basketClick() {
+    if ($basket.filter(product => product.id === item.id).length) {
+      // modify a product
+      basket.set(
+        $basket.map(product => {
+          if (product.id === item.id) {
+            product.qty++;
+            product.stock--;
+          }
+          return product;
+        })
+      );
+    } else {
+      // create a product
+      item.qty = item.qty ? item.qty++ : 1;
+      item.stock--;
+      basket.set([...$basket, item]);
+    }
+    stock = calculStock();
   }
 
   const dict = {
@@ -18,7 +49,7 @@
 
 <button
   class="btn btn-primary badge float-right"
-  data-badge={item.stock}
+  data-badge={stock}
   on:click|once={basketClick}>
   {dict.buy[$languageSelected]}
 </button>
