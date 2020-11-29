938b847c880df53faa014fd87192725039dca9e1
pntbr <pntbr@pm.me>
2020-11-29 09:47:57 +0100

    displays form in card

--- src/routes/adresse.svelte
+++ src/routes/adresse.svelte
@@ -1,8 +1,24 @@
 <script>
+  import { languageSelected } from "../stores.js";
   import AddressForm from "../components/AddressForm.svelte";
   import Address from "../components/displayAddress.svelte";
+
+  const dict = {
+    deliveryAddress: {
+      en: "Delivery Address",
+      fr: "Adresse de livraison"
+    }
+  };
 </script>
 
-<AddressForm />
+<div class="card">
+  <div class="card-header">
+    <div class="card-title h5">{dict.deliveryAddress[$languageSelected]}</div>
+  </div>
+  <div class="card-body">
+    <AddressForm />
+  </div>
+</div>
+
 <hr />
 <Address />
