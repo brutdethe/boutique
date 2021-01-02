d6f729cfd7cd8bd5f255526706f01213aae6dbb6
pntbr <pntbr@pm.me>
2021-01-02 22:58:48 +0100

    Adds shipping cost and sepa

--- src/routes/checkout-session-id.js
+++ src/routes/checkout-session-id.js
@@ -2,9 +2,8 @@ import Stripe from 'stripe'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const lineItems = []
 const checkoutData = {
-    payment_method_types: ['card'],
+    payment_method_types: ['card', 'sepa_debit'],
     billing_address_collection: 'auto',
     locale: 'fr',
     shipping_address_collection: {
@@ -16,25 +15,47 @@ const checkoutData = {
     cancel_url: 'https://fast-castle-84215.herokuapp.com/panier-annule'
 }
 
+
+const dict = {
+    shipping: {
+        en: "Shipping costs",
+        fr: "Frais de transport"
+    },
+}
+
 export async function post(req, res) {
     res.setHeader('Content-Type', 'application/json')
-    const items = req.body.basket;
+    const {
+        basket,
+        language,
+        shipping
+    } = req.body;
     const stripeSecret = process.env['stripe_secret']
     const stripe = new Stripe(stripeSecret)
 
     async function session() {
-        checkoutData.line_items = items.map(item => {
+        checkoutData.line_items = basket.map(item => {
             return {
                 price_data: {
                     currency: 'eur',
                     product_data: {
-                        name: item.titre[req.body.language],
+                        name: item.titre[language],
                     },
                     unit_amount: item.prix * 100,
                 },
                 quantity: item.qty,
             };
         });
+        checkoutData.line_items[checkoutData.line_items.length] = {
+            price_data: {
+                currency: 'eur',
+                product_data: {
+                    name: dict.shipping[language],
+                },
+                unit_amount: shipping * 100,
+            },
+            quantity: 1,
+        }
         return stripe.checkout.sessions.create(checkoutData);
     }
 
