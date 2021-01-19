953658cdcc8f2ccc29864eb8bf7aa244e2672991
pntbr <pntbr@pm.me>
2021-01-19 01:45:49 +0100

    change les urls de retoru stripe

--- src/routes/server/checkout-session-id.js
+++ src/routes/server/checkout-session-id.js
@@ -7,12 +7,12 @@ const checkoutData = {
     billing_address_collection: 'required',
     locale: '',
     shipping_address_collection: {
-        allowed_countries: ['FR', 'ES']
+        allowed_countries: ['FR', 'EN']
     },
     line_items: [],
     mode: 'payment',
-    success_url: 'http://shop.xn--brutdeth-i1a.fr/panier-ok?session_id={CHECKOUT_SESSION_ID}',
-    cancel_url: 'http://shop.xn--brutdeth-i1a.fr/panier-annule'
+    success_url: 'http://shop.xn--brutdeth-i1a.fr/fr/panier-ok?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url: 'http://shop.xn--brutdeth-i1a.fr/fr/panier-annule'
 }
 
 const dict = {
