70c4cb6eb2732c001a25de8a2da767be47e9402f
pntbr <pntbr@pm.me>
2021-01-31 16:37:54 +0100

    ajoute les mentions légales

--- src/routes/en/publisher.svelte
+++ src/routes/en/publisher.svelte
@@ -1,1 +1,39 @@
-PUBLISHER
+<Style
+  p {
+    max-width: 960px;
+  }
+</style>
+
+<h1>Legal notice</h1>.
+
+<p>
+  The site
+  <a href="http://www.theiere-tasse.com">http://www.theiere-tasse.com</a>
+  is edited by Jérôme Roussel auto-entrepreneur and hosted by
+  <a href="https://www.heroku.com/">Heroku</a>
+  . 
+</p>
+<p>
+  company listed under the registration number 451 185 953 and the code
+  APE 2341Z with the RM de Moulin and whose head office is located at 15, rue de Moulin, 15, rue de
+  perception, 03300 Cusset.
+</p>
+
+<p>
+  The site
+  <a href="http://www.theiere-tasse.com">http://www.theiere-tasse.com</a>
+  does not collect personal data.
+</p>
+
+<p>
+  Payments are made via
+  <a href="https://stripe.com/fr">Stripe</a>
+  :
+  <a href="https://stripe.com/fr/legal">Legal notice</a>
+
+</p>
+
+<p>
+  Director of publication: Mr. Jérôme Roussel, Managing Director:
+  <a href="mailto:jelepotier@laposte.net">jelepotier@laposte.net</a>
+</p>
---
+++ src/routes/fr/mentions-legales.svelte
@@ -0,0 +1,39 @@
+<style>
+  p {
+    max-width: 960px;
+  }
+</style>
+
+<h1>Mention légales</h1>
+
+<p>
+  Le site
+  <a href="http://www.theiere-tasse.com">http://www.theiere-tasse.com</a>
+  est édité par Jérôme Roussel auto-entrepreneur et hébergé par
+  <a href="https://www.heroku.com/">Heroku</a>
+  .
+</p>
+<p>
+  entreprise répertoriée sous le numéro d’immatriculation 451 185 953 et le code
+  APE 2341Z auprès du RM de Moulin et dont le siège social est sis au 15, rue de
+  la perception, 03300 Cusset.
+</p>
+
+<p>
+  Le site
+  <a href="http://www.theiere-tasse.com">http://www.theiere-tasse.com</a>
+  ne collecte pas de données personnelles.
+</p>
+
+<p>
+  Les paiement sont effectuées via
+  <a href="https://stripe.com/fr">Stripe</a>
+  :
+  <a href="https://stripe.com/fr/legal">Mentions légales</a>
+
+</p>
+
+<p>
+  Directeur de la publication : M. Jérôme Roussel, Gérant :
+  <a href="mailto:jelepotier@laposte.net">jelepotier@laposte.net</a>
+</p>
