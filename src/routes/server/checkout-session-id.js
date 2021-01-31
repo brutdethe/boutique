28f0a90a5300931106809c30c968fe0d395ce45f
pntbr <pntbr@pm.me>
2021-02-01 00:54:14 +0100

    test git on server

--- src/routes/server/checkout-session-id.js
+++ src/routes/server/checkout-session-id.js
@@ -5,6 +5,21 @@ import Stripe from 'stripe'
 
 import 'dotenv/config'
 
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function gh() {
+    const {
+        stdout,
+        stderr
+    } = await exec('git status');
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+}
+gh();
+
+
 let pages;
 
 const unsubscribe = pagesPath.subscribe(value => {
