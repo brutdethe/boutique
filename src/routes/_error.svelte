adf9c5076c24f1de63fee59143fb7e0038888bc0
pntbr <pntbr@pm.me>
2021-02-07 14:15:19 +0100

    place une erreur 404

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,40 +1,51 @@
 <script>
-	export let status;
-	export let error;
+  export let status;
+  export let error;
 
-	const dev = process.env.NODE_ENV === 'development';
+  const dev = process.env.NODE_ENV === "development";
 </script>
 
 <style>
-	h1, p {
-		margin: 0 auto;
-	}
+  .error {
+    text-align: center;
+    margin: auto;
+  }
+  h1,
+  p {
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  p {
+    margin: 1em auto;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>{status}</title>
+  <title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
+<section class="error">
+  <h1>{status}</h1>
 
-<p>{error.message}</p>
+  {#if status == '404'}
+    <img src="img/404.jpg" alt="404" />
+  {:else}
+    <p>{error.message}</p>
+  {/if}
 
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
-{/if}
+  {#if dev && error.stack}
+    <pre>{error.stack}</pre>
+  {/if}
+</section>
