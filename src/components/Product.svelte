aeec96f2bf2359486ff707a49983ec4b5d26294a
pntbr <pntbr@pm.me>
2021-05-16 22:43:37 +0200

    reprend l'aspect de la page produit

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -1,128 +1,199 @@
 <script>
-  import {
-    rate,
-    loadProducts,
-    categorySelected,
-    githubDataRepo
-  } from "../stores.js";
-  import Buy from "../components/Buy.svelte";
-  import Price from "../components/Price.svelte";
-  import Photo from "../components/Photo.svelte";
-  import ProductNotFound from "../components/ProductNotFound.svelte";
-  import Loading from "../components/Loading.svelte";
+	import { rate, loadProducts, categorySelected, githubDataRepo } from '../stores.js';
+	import Buy from '../components/Buy.svelte';
+	import Price from '../components/Price.svelte';
+	import Photo from '../components/Photo.svelte';
+	import ProductNotFound from '../components/ProductNotFound.svelte';
+	import Loading from '../components/Loading.svelte';
 
-  export let id;
-  export let lang;
+	export let id;
+	export let lang;
 
-  let product = loadProducts(id, $rate);
+	let product = loadProducts(id, $rate);
 
-  const dict = {
-    title: {
-      en: "product",
-      fr: "produit"
-    },
-    weight: {
-      en: "Weight",
-      fr: "Poids"
-    }
-  };
+	const dict = {
+		title: {
+			en: 'product',
+			fr: 'produit'
+		},
+		weight: {
+			en: 'Weight',
+			fr: 'Poids'
+		},
+		size: {
+			en: 'Size',
+			fr: 'Dimensions'
+		},
+		capacity: {
+			en: 'Capacity',
+			fr: 'Capacité'
+		}
+	};
 
-  const photosUrl = `https://raw.githubusercontent.com/${$githubDataRepo}/main/`;
+	const photosUrl = `https://raw.githubusercontent.com/${$githubDataRepo}/main/`;
 
-  function getWeight(weight, lang) {
-    if (lang === "en") {
-      return `${(weight * 35.274).toFixed(2)}oz`;
-    } else {
-      return `${weight * 1000}g`;
-    }
-  }
+	function getWeight(weight, lang) {
+		if (lang === 'en') {
+			return `${(weight * 35.274).toFixed(2)}oz`;
+		} else {
+			return `${weight * 1000}g`;
+		}
+	}
+
+	function getSize(size, lang) {
+		const sentence = [];
+		if ('largeur' in size) {
+			if (lang === 'en') {
+				sentence.push(`width: ${(size.largeur * 0.393701).toFixed(2)}in`);
+			} else {
+				sentence.push(`lg: ${size.largeur}cm`);
+			}
+		}
+		if ('hauteur' in size) {
+			if (lang === 'en') {
+				sentence.push(`height: ${(size.hauteur * 0.393701).toFixed(2)}in`);
+			} else {
+				sentence.push(`ht: ${size.hauteur}cm`);
+			}
+		}
+		return sentence.join(' x ');
+	}
+
+	function getStock(stock, lang) {
+		if (lang === 'en') {
+			return `${stock} item${stock > 1 ? 's' : ''} left`;
+		} else {
+			return `il reste ${stock} article${stock > 1 ? 's' : ''}`;
+		}
+	}
+
+	function getCapacity(capacity, lang) {
+		if (lang === 'en') {
+			return `${(capacity * 0.03381).toFixed(2)}oz`;
+		} else {
+			return `${capacity}ml`;
+		}
+	}
 </script>
 
 <style>
-  .carousel {
-    box-shadow: 0 0.25rem 1rem rgba(48, 55, 66, 0.15);
-  }
+	.card-carousel {
+		padding: 0;
+	}
 
-  .carousel .carousel-container::before {
-    padding-bottom: 66%;
-  }
+	.carousel {
+		box-shadow: 0 0.25rem 1rem rgba(48, 55, 66, 0.15);
+		width: 100%;
+	}
 
-  .price {
-    padding-top: 0.8rem;
-    text-align: right;
-    font-weight: 700;
-  }
+	.carousel .carousel-container::before {
+		padding-bottom: 66%;
+	}
 
-  .btn {
-    width: 5rem;
-  }
+	dt,
+	dd {
+		float: left;
+		margin: 0;
+	}
+
+	dt {
+		clear: both;
+	}
+
+	.price {
+		padding-top: 0.8rem;
+		text-align: right;
+		font-weight: 700;
+	}
+
+	.btn {
+		width: 5rem;
+	}
 </style>
 
 <svelte:head>
-  <title>{dict.title[lang]}-{id}</title>
+	<title>{dict.title[lang]}-{id}</title>
 </svelte:head>
 {#if $product}
-  {#if $product.hasOwnProperty('id')}
-    <div class="card">
-      {#if $product.photos.length}
-        <div class="card-image carousel">
-          {#each $product.photos as photo, count}
-            <input
-              class="carousel-locator"
-              id="slide-{count + 1}"
-              type="radio"
-              name="carousel-radio"
-              hidden="true"
-              checked={!count && true} />
-          {/each}
-          <div class="carousel-container">
-            {#each $product.photos as photo, count}
-              <figure class="carousel-item">
-                <label
-                  class="item-prev btn btn-action btn-lg"
-                  for="slide-{count ? count : $product.photos.length}">
-                  <i class="icon icon-arrow-left" />
-                </label>
-                <label
-                  class="item-next btn btn-action btn-lg"
-                  for="slide-{count + 2 <= $product.photos.length ? count + 2 : 1}">
-                  <i class="icon icon-arrow-right" />
-                </label>
-                <Photo
-                  alt={`${$product.titre[lang]} #${$product.id}`}
-                  {lang}
-                  url={`carousels/${photo}`} />
+	{#if $product.hasOwnProperty('id')}
+		<div class="columns">
+			<div class="card column col-8 col-xs-12 card-carousel">
+				{#if $product.photos.length}
+					<div class="card-image carousel">
+						{#each $product.photos as photo, count}
+							<input
+								class="carousel-locator"
+								id="slide-{count + 1}"
+								type="radio"
+								name="carousel-radio"
+								hidden="true"
+								checked={!count && true} />
+						{/each}
+						<div class="carousel-container">
+							{#each $product.photos as photo, count}
+								<figure class="carousel-item">
+									<label
+										class="item-prev btn btn-action btn-lg"
+										for="slide-{count ? count : $product.photos.length}">
+										<i class="icon icon-arrow-left" />
+									</label>
+									<label
+										class="item-next btn btn-action btn-lg"
+										for="slide-{count + 2 <= $product.photos.length ? count + 2 : 1}">
+										<i class="icon icon-arrow-right" />
+									</label>
+									<Photo
+										alt={`${$product.titre[lang]} #${$product.id}`}
+										{lang}
+										url={`carousels/${photo}`} />
 
-              </figure>
-            {/each}
-          </div>
-          <div class="carousel-nav">
-            {#each $product.photos as photo, count}
-              <label class="nav-item text-hide c-hand" for="slide-{count + 1}">
-                {count}
-              </label>
-            {/each}
-          </div>
-        </div>
-      {/if}
-      <div class="card-header">
-        <div class="card-title h4">{$product.titre[lang]}</div>
-      </div>
-      <div class="card-body">
-        <p>{$product.description[lang]}.</p>
-        <p>{dict.weight[lang]} : {getWeight($product.poids, lang)}</p>
-        <p>Stock : {$product.stock}</p>
-        <h3 class="card-title h1 price">
-          <Price price={$product.prix} />
-        </h3>
-      </div>
-      <div class="card-footer">
-        <Buy item={$product} {lang} />
-      </div>
-    </div>
-  {:else}
-    <Loading />
-  {/if}
+								</figure>
+							{/each}
+						</div>
+						<div class="carousel-nav">
+							{#each $product.photos as photo, count}
+								<label class="nav-item text-hide c-hand" for="slide-{count + 1}">{count}</label>
+							{/each}
+						</div>
+					</div>
+				{/if}
+
+			</div>
+			<div class="card column col-4 col-xs-12">
+				<div class="card-header">
+					<div class="card-title h4">{$product.titre[lang]}</div>
+				</div>
+				<div class="card-body">
+					<p>{$product.description[lang]}.</p>
+					<dl>
+						{#if 'capacite' in $product}
+							<dt>{dict.capacity[lang]} :&nbsp;</dt>
+							<dd>{getCapacity($product.capacite, lang)}</dd>
+						{/if}
+						{#if 'poids' in $product}
+							<dt>{dict.weight[lang]} :&nbsp;</dt>
+							<dd>{getWeight($product.poids, lang)}</dd>
+						{/if}
+						{#if 'dimension' in $product}
+							<dt>{dict.size[lang]} :&nbsp;</dt>
+							<dd>{getSize($product.dimension, lang)}</dd>
+						{/if}
+						<dt>Stock :&nbsp;</dt>
+						<dd>{getStock($product.stock, lang)}</dd>
+					</dl>
+
+				</div>
+				<div class="card-footer">
+					<h3 class="card-title h1 price">
+						<Price price={$product.prix} />
+					</h3>
+					<Buy item={$product} {lang} />
+				</div>
+			</div>
+		</div>
+	{:else}
+		<Loading />
+	{/if}
 {:else}
-  <ProductNotFound {lang} />
+	<ProductNotFound {lang} />
 {/if}
