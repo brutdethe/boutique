6c51990f9931f22bac9203d3b1da7e1cf33d62f6
pntbr <pntbr@pm.me>
2021-05-29 15:07:03 +0200

    ajoute le logo et centre la nav

--- src/components/Currency.svelte
+++ src/components/Currency.svelte
@@ -1,36 +1,37 @@
 <script>
-    import {currency} from "../stores.js";
+	import { currency } from '../stores.js';
 
-    function changeCurrencySelected(evt) {
-        currency.set($currency === "EUR" ? "USD" : "EUR");
-    }
+	function changeCurrencySelected(evt) {
+		currency.set($currency === 'EUR' ? 'USD' : 'EUR');
+	}
 </script>
 
 <style>
-    .currency {
-        flex-wrap: nowrap;
-        line-height: 4em;
-    }
+	.currency {
+		flex-wrap: nowrap;
+		line-height: 4em;
+		text-align: left;
+	}
 
-    @media (max-width: 600px) {
-        .currency {
-            text-align: right;
-            padding-right: 10px !important;
-        }
-    }
+	@media (max-width: 600px) {
+		.currency {
+			text-align: left;
+			padding-right: 10px !important;
+		}
+	}
 </style>
 
-<div class="currency column col-5">
-    <button
-            class="btn btn-sm {'EUR' === $currency ? 'bg-secondary' : ''}"
-            on:click={changeCurrencySelected}
-            value="EUR">
-        €
-    </button>
-    <button
-            class="btn btn-sm {'USD' === $currency ? 'bg-secondary' : ''}"
-            on:click={changeCurrencySelected}
-            value="USD">
-        $
-    </button>
+<div class="currency column col-6">
+	<button
+		class="btn btn-sm {'EUR' === $currency ? 'bg-secondary' : ''}"
+		on:click={changeCurrencySelected}
+		value="EUR">
+		€
+	</button>
+	<button
+		class="btn btn-sm {'USD' === $currency ? 'bg-secondary' : ''}"
+		on:click={changeCurrencySelected}
+		value="USD">
+		$
+	</button>
 </div>
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -21,18 +21,30 @@
 
 <style>
 	header {
+		max-width: 90em;
+		margin: auto;
 		align-items: center;
 		border-bottom: 1px solid rgba(255, 62, 0, 0.1);
 		font-weight: 300;
-		padding: 0 1em;
+		padding: 0 2em;
 	}
 
 	.title {
 		margin-bottom: 0;
+		padding: 0;
 		font-size: 2rem;
 		font-weight: 700;
 		line-height: 1.5;
 		color: #5755d9;
+	}
+
+	.logo {
+		width: 6rem;
+		margin-left: -8px;
+	}
+
+	.i18n {
+		padding: 0;
 	}
 
 	ul {
@@ -71,10 +83,11 @@
 </style>
 
 <header class="columns">
-	<h1 class="title column col-4 col-md-12">
-		<a href="/">{$setup.nom || ''}</a>
+	<h1 class="title column col-1 col-md-12">
+		<!-- if -->
+		<img src="/sceau-the.svg" alt="logo boutique" class="logo" />
 	</h1>
-	<nav class="column col-5 col-md-9 col-sm-12">
+	<nav class="column col-9 col-md-9 col-sm-12">
 		<ul>
 			<li>
 				<a href={$pagesPath.index[lang]}>{dict.nav_products[lang]}</a>
@@ -93,7 +106,7 @@
 	</nav>
 
 	<!-- TOUTDOUX: aligner à droite-->
-	<div class="columns col-gapless column col-3 col-md-3 col-sm-12">
+	<div class="i18n columns col-gapless column col-2 col-md-3 col-sm-12">
 		<Currency />
 		<Language />
 	</div>
