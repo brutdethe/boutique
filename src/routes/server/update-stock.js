b64c62abc4bfa53ab1d17394cba53bc25af8f4cb
pntbr <pntbr@pm.me>
2022-10-12 21:25:02 +0200

    Enlève les requète à stripe

--- src/routes/server/update-stock.js
+++ src/routes/server/update-stock.js
@@ -1,55 +1,13 @@
 import fetch from 'node-fetch'
-import Stripe from 'stripe'
 import 'dotenv/config'
 
-async function getPaidsSessionsIds(stripe) {
-    const checkoutSessions = await stripe.checkout.sessions.list()
-    return checkoutSessions.data.filter(session => session.payment_status === 'paid').map(session => session.id)
-}
 
-async function getSessionsItems(stripe, sessionIds) {
-    return Promise.all(sessionIds.map(sessionId => stripe.checkout.sessions.listLineItems(sessionId)))
-}
-
-function getItems(sessionsItems) {
-    return sessionsItems
-        .map(items => items.data)
-        .flat()
-        .filter(item => item.description.match(/id:.*$/))
-        .map(item => ({
-            id: item.description.match(/id:(.*)$/)[1],
-            quantity: item.quantity
-        }))
-}
-
-function getItemsGroupBy(items) {
-    const counts = items.reduce((prev, curr) => {
-        let count = prev.get(curr.id) || 0
-        prev.set(curr.id, +curr.quantity + count)
-        return prev
-    }, new Map())
-
-    return [...counts].map(([id, quantity]) => ({
-        id,
-        quantity
-    }))
-}
-
-function updatesStock(productsJSON, productsSession) {
+function updatesStock(productsJSON) {
     let productsUpdated = productsJSON
 
     productsUpdated = productsUpdated.map(product => {
         product.stock = product['quantité_produite']
         return product
-    })
-
-    productsSession.forEach(item => {
-        productsUpdated = productsUpdated.map(product => {
-            if (item.id === product.id) {
-                product.stock -= item.quantity
-            }
-            return product
-        })
     })
 
     return productsUpdated
@@ -60,19 +18,14 @@ export async function post(req, res) {
 
     const ghDataRepo = process.env.github_data_repo
     const productsPath = `https://raw.githubusercontent.com/${ghDataRepo}/main/produits.json`
-    const stripeKeySk = process.env['stripe_sk']
-    const stripe = new Stripe(stripeKeySk)
 
     try {
-        const paidsSessionsIds = await getPaidsSessionsIds(stripe)
-        const sessionsItems = await getSessionsItems(stripe, paidsSessionsIds)
-        const items = getItems(sessionsItems)
-        const itemsGroupBy = getItemsGroupBy(items)
+
 
         const productsJSON = await fetch(productsPath).then(result => result.json())
-        const productsUpdated = updatesStock(productsJSON, itemsGroupBy)
+        updatesStock(productsJSON)
 
-        return res.end(JSON.stringify(productsUpdated))
+        return res.end(JSON.stringify(productsJSON))
     } catch (err) {
         console.log('No such checkout stripe session', err)
 
